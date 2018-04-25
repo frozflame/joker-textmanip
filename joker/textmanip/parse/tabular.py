ad1281a41bd35cee552d9e5bd4f6f463834ce18b
frozflame <frozflame@outlook.com>
2018-04-25 15:06:28 +0800

    rename tabdelim_to_dict to two_columns_to_dict

--- joker/textmanip/parse/tabular.py
+++ joker/textmanip/parse/tabular.py
@@ -7,10 +7,9 @@ from __future__ import division, print_function
 from collections import OrderedDict
 
 
-def tabdelim_to_dict(text, reverse=False):
+def two_columns_to_dict(text, reverse=False):
     tups = [lx.strip().split() for lx in text.splitlines()]
     if reverse:
         tups = [tu[::-1] for tu in tups]
     return OrderedDict(tups)
-
 
