63366c94c090f10280565177dc90c78b467f01b8
frozflame <frozflame@outlook.com>
2018-08-22 14:23:52 +0800

    merge routine.py into misc.py

--- joker/textmanip/misc.py
+++ joker/textmanip/misc.py
@@ -3,12 +3,14 @@
 
 from __future__ import division, print_function
 
+import datetime
 import random
-
+import sys
 from string import digits, ascii_letters, ascii_uppercase
 
-
 # http://www.garykessler.net/library/base64.html
+from joker.textmanip.path import smart_extension_join
+
 b64_chars = digits + ascii_letters + '+/'
 b64_urlsafe_chars = ascii_letters + digits + '_-'
 b32_chars = ascii_uppercase + '234567'
@@ -19,3 +21,23 @@ def random_string(length, chars=None):
         chars = digits + ascii_letters
     return ''.join(random.choice(chars) for _ in range(length))
 
+
+def text_routine_catstyle(func):
+    # routine function for quick and dirty text manipulation scripts
+    if not sys.argv[1:]:
+        for line in sys.stdin:
+            print(func(line))
+        return
+    for path in sys.argv[1:]:
+        for line in open(path):
+            print(func(line))
+
+
+def text_routine_perfile(func, ext=None):
+    # routine function for quick and dirty text manipulation scripts
+    if ext is None:
+        ext = '.out_{}'.format(datetime.datetime.now())
+
+    for path in sys.argv[1:]:
+        outpath = smart_extension_join(path, ext)
+        func(path, outpath)
