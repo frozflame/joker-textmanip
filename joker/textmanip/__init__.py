ca61e4bddce20a2d051ffab859e7d57e22166933
Hailong <lendoli@163.com>
2018-11-02 00:22:36 +0800

    remove_emptylines, dedup_spaces, proper_join

--- joker/textmanip/__init__.py
+++ joker/textmanip/__init__.py
@@ -3,12 +3,11 @@
 
 __version__ = '0.0.6'
 
+b32_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
 b64_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'
 
 b64_urlsafe_chars = \
     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-'
-
-b32_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
 
 
 def random_string(length, chars=None):
@@ -17,22 +16,56 @@ def random_string(length, chars=None):
     return ''.join(random.choice(chars) for _ in range(length))
 
 
-def remove_chars(s, chars):
+def remove_chars(text, chars):
     """
-    :param s: (str)
+    :param text: (str)
     :param chars: (str or list) characters to be removed
     :return: (str)
     """
-    return s.translate(dict.fromkeys(ord(c) for c in chars))
+    return text.translate(dict.fromkeys(ord(c) for c in chars))
 
 
-def remove_control_chars(s):
-    return s.translate(dict.fromkeys(range(32)))
+def remove_control_chars(text):
+    return text.translate(dict.fromkeys(range(32)))
 
 
-def remove_whitespaces(s):
-    return ''.join(s.split())
+def remove_whitespaces(text):
+    return ''.join(text.split())
 
 
-def remove_newlines(s):
-    return ' '.join(s.splitlines())
+def remove_newlines(text):
+    # similar to VIM line join
+    return ' '.join(text.splitlines())
+
+
+def remove_emptylines(text):
+    lines = text.splitlines(keepends=True)
+    return ''.join(x for x in lines if x.strip())
+
+
+def replace_newlines(text, nl='\n'):
+    text = text.replace('\n\r', nl)
+    text = text.replace('\r', nl)
+    if nl != '\n':
+        text = text.replace('\n', nl)
+    return text
+
+
+def dedup_spaces(text):
+    import re
+    return re.sub(r" {2,}", " ", text)
+
+
+def proper_join(parts):
+    import re
+    regex = re.compile(r'\s$')
+    _parts = []
+    space = chr(32)
+    for p in parts:
+        if not p:
+            continue
+        if regex.search(p):
+            _parts.append(p)
+        else:
+            _parts.append(p + space)
+    return ''.join(_parts)
