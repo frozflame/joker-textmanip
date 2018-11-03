2f4fdf6f2ef3ad49eabfeb48f657cbd8635232b0
frozflame <frozflame@outlook.com>
2018-11-03 12:35:49 +0800

    rename chinese to cjk: remove cjk funcs and chinese num funcs

--- joker/textmanip/regex.py
+++ joker/textmanip/regex.py
@@ -61,30 +61,3 @@ def make_range_pattern(blocks):
     return ''.join(parts)
 
 
-cjk_blocks = [
-    (0x2E80, 0x2EFF, "CJK Radicals Supplement"),
-    (0x3000, 0x303F, "CJK Symbols & Punctuation"),
-    (0x31C0, 0x31EF, "CJK Strokes"),
-    (0x3200, 0x32FF, "CJK Enclosed Letters and Months"),
-    (0x3300, 0x33FF, "CJK Compatibility"),
-    (0x3400, 0x4DBF, "CJK Unified Ideographs Extension A"),
-    (0x4E00, 0x9FFF, "CJK Unified Ideographs"),
-    (0xF900, 0xFAFF, "CJK Compatibility Ideographs"),
-    (0xFE30, 0xFE4F, "CJK Compatibility Forms"),
-]
-
-
-def remove_cjk(text):
-    cjk = make_range_pattern(cjk_blocks)
-    return re.sub('[{}]'.format(cjk), '', text)
-
-
-def remove_spaces_beside_cjk(text):
-    cjk = make_range_pattern(cjk_blocks)
-    parts = re.split(r'\s*([{}]+)\s*'.format(cjk), text)
-    return ''.join(parts)
-
-
-def remove_spaces_between_cjk(text):
-    cjk = make_range_pattern(cjk_blocks)
-    return re.sub(r'([{0}]+)\s+(?=[{0}])'.format(cjk), r'\1', text)
