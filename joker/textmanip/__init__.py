f32437ebb2f7be15f8306f8400e25f09d9de816e
Hailong <lendoli@163.com>
2018-11-05 14:36:32 +0800

    ver 0.1.0, __version__ set

--- joker/textmanip/__init__.py
+++ joker/textmanip/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-__version__ = '0.0.6'
+__version__ = '0.1.0'
 
 b32_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
 b64_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'
