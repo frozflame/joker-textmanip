2baa9d5ed7c436633607417e456c72f0d7a1d88d
frozflame <frozflame@outlook.com>
2019-08-04 15:58:49 +0800

    Stream, ShellStream

--- joker/textmanip/stream.py
+++ joker/textmanip/stream.py
@@ -3,9 +3,11 @@
 
 from __future__ import division, print_function
 
+import io
 import os
-import time
 import sys
+import time
+import weakref
 
 
 def _iter_lines(path, *args, **kwargs):
@@ -54,6 +56,109 @@ def write_lines(lines, path, mode='w', *args, **kwargs):
         _write_lines(lines, path, mode, *args, **kwargs)
 
 
+class Stream(object):
+    __slots__ = ['file']
+    opened = weakref.WeakValueDictionary()
+    _preopened = {
+        (1, 'w'): sys.stdout,
+        (2, 'w'): sys.stderr,
+        ('', 'r'): sys.stdin,
+        ('', 'w'): sys.stdout,
+        ('-', 'r'): sys.stdin,
+        ('-', 'w'): sys.stdout,
+        ('<stdin>', 'r'): sys.stdin,
+        ('<stdout>', 'w'): sys.stdout,
+        ('<stderr>', 'w'): sys.stderr,
+    }
+    _safe_attributes = {'mode', 'name'}
+
+    @classmethod
+    def open(cls, file, mode='r', *args, **kwargs):
+        k = file, mode
+        f = cls._preopened.get(k)
+        if f is None:
+            f = open(file, mode, *args, **kwargs)
+            cls.opened[id(f)] = f
+        return cls(f)
+
+    @classmethod
+    def wrap(cls, content):
+        if isinstance(content, str):
+            return cls(io.StringIO(content))
+        if isinstance(content, bytes):
+            return cls(io.BytesIO(content))
+        return cls(io.StringIO(str(content)))
+
+    def __init__(self, file):
+        self.file = file
+
+    def __iter__(self):
+        return iter(self.file)
+
+    def __getattr__(self, name):
+        if name in self._safe_attributes:
+            return getattr(self.file, name, None)
+        return getattr(self.file, name)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if id(self.file) in self.opened:
+            self.file.__exit__(exc_type, exc_val, exc_tb)
+
+    def is_binary(self):
+        if self.mode:
+            return 'b' in self.mode
+        try:
+            return isinstance(self.file.read(0), bytes)
+        except Exception:
+            pass
+
+
+class ShellStream(Stream):
+    def lines(self):
+        for line in self.file:
+            yield line.strip()
+
+    def nonblank_lines(self):
+        for line in self.file:
+            line = line.strip()
+            if line:
+                yield line
+
+    def ungrep(self, pattern, flags=0):
+        import re
+        for line in self.file:
+            if re.search(pattern, line, flags) is None:
+                yield line
+
+    def grep(self, pattern, flags=0, group=None):
+        import re
+        for line in self.file:
+            mat = re.search(pattern, line, flags)
+            if mat is None:
+                continue
+            if group is None:
+                yield line
+            else:
+                yield mat.group(group)
+
+    def quote(self, strip=False):
+        import shlex
+        if self.is_binary():
+            raise TypeError('file should be opened in text mode')
+        if strip in ('l', 'r'):
+            func = getattr(str, str(strip) + 'strip')
+            lines = (func(line) for line in self.file)
+        elif strip:
+            lines = (line.strip() for line in self.file)
+        else:
+            lines = (line.rstrip('\n') for line in self.file)
+        for line in lines:
+            yield shlex.quote(line)
+
+
 class AtomicTailer(object):
     """
     Read log file on-line
@@ -83,7 +188,6 @@ class AtomicTailer(object):
     def follow(self):
         """\
         follow a growing file
-
         tldr: http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/157035
         """
         tm = time.time()
